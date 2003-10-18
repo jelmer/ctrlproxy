55fb74ab48344025de27aa79b0f8ea768b15b656
jelmer <>
2003-10-18 23:55:01 +0200

    New update

--- ctrlproxy.h
+++ ctrlproxy.h
@@ -20,82 +20,203 @@
 #ifndef __CTRLPROXY_H__
 #define __CTRLPROXY_H__
 
-#define IRC_MSG_LEN 540
-#define MAXHOSTNAMELEN 4096
-#define CTRLPROXY_VERSION "0.6"
-
 #include <unistd.h>
 #include <time.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdarg.h>
+#include <glib.h>
+#include <gmodule.h>
+#include <libxml/xmlmemory.h>
+#include <libxml/parser.h>
+#include <sys/select.h>
 
-extern char output_debug;
-
-#define DEBUG if(output_debug)printf
-
-struct server;
+struct network;
+struct client;
 struct line;
+struct transport_context;
 
-extern char my_hostname[MAXHOSTNAMELEN+2];
+enum data_direction { UNKNOWN = 0, TO_SERVER = 1, FROM_SERVER = 2 };
 
-int server_send_raw(struct server *s, char *data);
-int server_send(struct server *s, char *origin, ...);
+typedef void (*disconnect_handler) (struct transport_context *, void *data);
+typedef void (*receive_handler) (struct transport_context *, char *l, void *data);
+typedef void (*newclient_handler) (struct transport_context *, struct transport_context *, void *data);
 
-/* conf.c */
-char **enum_sections(void);
-char *get_conf(char *section, char *name);
+struct transport {
+	char *name;
+	/* The connect and listen functions should add something to the poll */
+	int (*connect) (struct transport_context *context);
+	int (*listen) (struct transport_context *context);
+	int (*write) (struct transport_context *context, char *l);
+	int (*close) (struct transport_context *context);
+	int reference_count;
+};
 
-/* util.c */
-void init_sockaddr (struct sockaddr_in *name, const char *hostname, uint16_t port);
-int make_socket (uint16_t port);
-int aread(int fd, char **remaining);
-char *anextline(char **remaining);
-int irc_parse_line(char *data, struct line *l);
+struct transport_context {
+	struct transport *functions;
+	xmlNodePtr configuration;
+	void *data;
+	void *caller_data;
+	disconnect_handler on_disconnect;
+	receive_handler on_receive;
+	newclient_handler on_new_client;
+};
 
 struct line {
-	char raw[IRC_MSG_LEN];
-	char data[IRC_MSG_LEN];
-	char *origin;
-	char *args[IRC_MSG_LEN]; /* NULL terminated */
+	enum data_direction direction;
+	int options;
+	struct network *network;
+	struct client *client;
+	const char *origin;
+	char **args; /* NULL terminated */
+	size_t argc;
 };
 
-struct server {
-	struct server *prev, *next;
+/* for the options fields */
+#define LINE_IS_PRIVATE      1 
+#define LINE_DONT_SEND       2 
+#define LINE_STOP_PROCESSING 4
+
+struct nick {
 	char *name;
-	char *abbrev;
-	char *username;
-	char *fullname;
+	char mode;
+};
+
+struct channel {
+	xmlNodePtr xmlConf;
+	char *topic;
+	char mode; /* Private, secret, etc */
+	char *modes[255];
+	char introduced;
+	char namreply_started;
+	long limit;
+	GList *nicks;
+};
+
+struct client {
+	struct network *network;
+	char authenticated;
+	struct transport_context *incoming;
+	time_t connect_time;
+	char did_nick_change;
+};
+
+struct network { 
+	xmlNodePtr xmlConf;
+	char modes[255];
+	xmlNodePtr servers;
 	char *hostmask;
-	char *password;
-	char *nick;
-	time_t reconnect_time;
-	time_t ping_time;
-	time_t last_msg_time;
-	int port;
-	int socket;
-	struct module_context *handlers;
-	char *remaining;
+	GList *channels;
+	char authenticated;
+	GList *clients;
+	xmlNodePtr current_server;
+	xmlNodePtr listen;
+	void  *replication_data;
+	char *supported_modes[2];
+	char **features;
+	struct transport_context *outgoing;
+	struct transport_context **incoming;
 };
 
-struct module_context {
-	struct module_context *prev, *next;
-	struct server *parent;
-	struct module_functions *functions;
-	char closed;
-	void *private_data;
-	void *handle;
+struct plugin {
+	char *name;
+	GModule *module;		
+	xmlNodePtr xmlConf;
+	void *data;
 };
 
-struct module_functions {
-	void (*init)(struct module_context *);
-	void (*loop)(struct module_context *);
-	void (*handle_incoming_data)(struct module_context *, const struct line *);
-	void (*handle_outgoing_data)(struct module_context *, const struct line *);
-	void (*fini)(struct module_context *);
-	struct module_functions *prev, *next;
+typedef gboolean (*plugin_init_function) (struct plugin *);
+typedef gboolean (*plugin_fini_function) (struct plugin *);
+extern void (*replicate_function) (struct network *, struct transport_context *);
+
+/* state.c */
+struct channel *find_channel(struct network *st, char *name);
+struct nick *find_nick(struct channel *c, char *name);
+GSList *gen_replication(struct network *s);
+
+/* server.c */
+struct network *connect_to_server(xmlNodePtr);
+int close_server(struct network *s);
+void default_replicate_function (struct network *, struct transport_context *);
+extern GList *networks;
+void clients_send(struct network *, struct line *, struct transport_context *exception);
+void network_add_listen(struct network *, xmlNodePtr);
+
+/* line.c */
+struct line *linedup(struct line *l);
+struct line * irc_parse_line(char *data);
+struct line * virc_parse_line(char *origin, va_list ap);
+char *irc_line_string(struct line *l);
+char *irc_line_string_nl(struct line *l);
+char *line_get_nick(struct line *l);
+void free_line(struct line *l);
+gboolean irc_send_args(struct transport_context *, ...);
+gboolean irc_sendf(struct transport_context *, char *fmt, ...);
+int irc_send_line(struct transport_context *, struct line *l);
+struct line *irc_parse_linef( char *origin, ... );
+struct line *irc_parse_line_args( char *origin, ... );
+
+/* main.c */
+void clean_exit();
+void save_configuration();
+gboolean load_plugin(xmlNodePtr);
+gboolean unload_plugin(struct plugin *);
+gboolean plugin_loaded(char *name);
+
+/* transport.c */
+void register_transport(struct transport *);
+gboolean unregister_transport(char *name);
+struct transport_context *transport_connect(const char *name, xmlNodePtr p, receive_handler, disconnect_handler, void *data);
+struct transport_context *transport_listen(const char *name, xmlNodePtr p, newclient_handler, void *data);
+void transport_free(struct transport_context *);
+int transport_write(struct transport_context *, char *l);
+void transport_set_disconnect_handler(struct transport_context *, disconnect_handler);
+void transport_set_receive_handler(struct transport_context *, receive_handler);
+void transport_set_newclient_handler(struct transport_context *, newclient_handler);
+void transport_set_data(struct transport_context *, void *);
+
+/* linestack.c */
+struct linestack_context;
+struct linestack {
+	char *name;
+	gboolean (*init) (struct linestack_context *, char *args);
+	gboolean (*clear) (struct linestack_context *);
+	gboolean (*add_line) (struct linestack_context *, struct line *);
+	GSList *(*get_linked_list) (struct linestack_context *);
+	void (*send) (struct linestack_context *, struct transport_context *);
+	gboolean (*destroy) (struct linestack_context *);
 };
 
-#define CTRLPROXY_INTERFACE_VERSION 0
-#define CTRLPROXY_VERSIONING_MAGIC int ctrlproxy_module_version = CTRLPROXY_INTERFACE_VERSION;
+struct linestack_context {
+	struct linestack *functions;
+	void *data;
+};
+
+void register_linestack(struct linestack *);
+void unregister_linestack(struct linestack *);
+struct linestack_context *linestack_new(char *name, char *args);
+GSList *linestack_get_linked_list(struct linestack_context *);
+void linestack_send(struct linestack_context *, struct transport_context *);
+gboolean linestack_destroy(struct linestack_context *);
+gboolean linestack_clear(struct linestack_context *);
+gboolean linestack_add_line(struct linestack_context *, struct line *);
+gboolean linestack_add_line_list(struct linestack_context *, GSList *);
+
+/* util.c */
+char *list_make_string(char **);
+
+xmlNodePtr xmlFindChildByName(xmlNodePtr parent, const xmlChar *name);
+xmlNodePtr xmlFindChildByElementName(xmlNodePtr parent, const xmlChar *name);
+extern xmlNodePtr xmlNode_networks, xmlNode_plugins;
+extern GList *plugins;
+extern xmlDocPtr configuration;
+
+int verify_client(struct network *s, struct client *c);
+
+/* hooks.c */
+/* Returns TRUE if filter should be continued, FALSE if it should be stopped. */
+typedef gboolean (*filter_function) (struct line *);
+void add_filter(char *name, filter_function);
+void del_filter(filter_function);
 
 #endif /* __CTRLPROXY_H__ */
