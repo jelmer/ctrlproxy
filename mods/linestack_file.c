6ea0c1ddf8e7c991a6992e8c817dcac52cafc42e
jelmer <>
2003-12-13 15:38:18 +0200

    Fixes

--- mods/linestack_file.c
+++ mods/linestack_file.c
@@ -23,56 +23,68 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+struct file_information {
+	GIOChannel *channel;
+	char *filename;
+};
+
 static gboolean file_init(struct linestack_context *c, char *args)
 {
+	struct file_information *d = malloc(sizeof(struct file_information));
 	GError *error = NULL;
 	if(args) {
-		c->data = g_io_channel_new_file(args, "a+", NULL);
+		d->channel = g_io_channel_new_file(args, "a+", NULL);
+		d->filename = strdup(args);
 	} else  {
 		int tmpfd;
 		char *tmpname;
 		char *p = ctrlproxy_path("linestack_file");
 		mkdir(p, 0700);
-		asprintf(&tmpname, "%s/XXXXXX", p);
+		asprintf(&d->filename, "%s/XXXXXX", p);
 		free(p);
-		tmpfd = g_mkstemp(tmpname);
-		free(tmpname);
+		tmpfd = g_mkstemp(d->filename);
 		if(tmpfd < 0) return FALSE;
 
-		c->data = g_io_channel_unix_new(tmpfd);
+		d->channel = g_io_channel_unix_new(tmpfd);
 	}
 
-	g_io_channel_set_encoding((GIOChannel *)c->data, NULL, &error);
+	g_io_channel_set_encoding(d->channel, NULL, &error);
 
-	if(!c->data) return FALSE;
+	c->data = d;
+
+	if(!c->data || !d->channel) return FALSE;
 	return TRUE;
 }
 
 static gboolean file_clear(struct linestack_context *c)
 {
-	GIOChannel *io = c->data;
-	g_io_channel_seek_position(io, 0, G_SEEK_SET, NULL);
+	struct file_information *d = (struct file_information *)c->data;
+	g_io_channel_seek_position(d->channel, 0, G_SEEK_SET, NULL);
 	return TRUE;
 }
 
 static gboolean file_close(struct linestack_context *c)
 {
-	GIOChannel *io = c->data;
-	g_io_channel_shutdown(io, TRUE, NULL);
+	struct file_information *d = (struct file_information *)c->data;
+	g_io_channel_shutdown(d->channel, TRUE, NULL);
+	unlink(d->filename);
+	free(d->filename);
+	free(d);
+	c->data = NULL;
 	return TRUE;
 }
 
 static GSList *file_get_linked_list(struct linestack_context *c)
 {
-	GIOChannel *io = (GIOChannel *)c->data;
+	struct file_information *d = (struct file_information *)c->data;
 	GError *error = NULL;
 	GSList *ret = NULL;
 	char *raw;
 
 	/* Go back to begin of file */
-	g_io_channel_seek(io, 0, G_SEEK_SET);
+	g_io_channel_seek(d->channel, 0, G_SEEK_SET);
 	
-	while(g_io_channel_read_line(io, &raw, NULL, NULL, &error) == G_IO_STATUS_NORMAL) {
+	while(g_io_channel_read_line(d->channel, &raw, NULL, NULL, &error) == G_IO_STATUS_NORMAL) {
 		struct line *l;
 		l = irc_parse_line(raw);
 		free(raw);
@@ -85,25 +97,25 @@ static GSList *file_get_linked_list(struct linestack_context *c)
 
 static gboolean file_add_line(struct linestack_context *b, struct line *l)
 {
-	GIOChannel *io = (GIOChannel *)b->data;
+	struct file_information *d = (struct file_information *)b->data;
 	GError *error = NULL;
 	char *raw = irc_line_string_nl(l);
-	g_assert(io);
-	g_io_channel_write_chars(io, raw, -1, NULL, &error);
+	g_assert(d->channel);
+	g_io_channel_write_chars(d->channel, raw, -1, NULL, &error);
 	free(raw);
 	return TRUE;
 }	
 
 static void file_send_file(struct linestack_context *b, struct transport_context *t) 
 {
-	GIOChannel *io = (GIOChannel *)b->data;
+	struct file_information *d = (struct file_information *)b->data;
 	char *raw;
 	GError *error = NULL;
 	
 	/* Go back to begin of file */
-	g_io_channel_seek(io, 0, G_SEEK_SET);
+	g_io_channel_seek(d->channel, 0, G_SEEK_SET);
 	
-	while(g_io_channel_read_line(io, &raw, NULL, NULL, &error) == G_IO_STATUS_NORMAL)  {
+	while(g_io_channel_read_line(d->channel, &raw, NULL, NULL, &error) == G_IO_STATUS_NORMAL)  {
 		transport_write(t, raw);
 		free(raw);
 	}
