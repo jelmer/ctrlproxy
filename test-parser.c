4162e8d1d891eafe714d9f0a964e50650ab0fc00
jelmer <>
2003-12-18 23:05:50 +0200

    Add tester

---
+++ test-parser.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "ctrlproxy.h"
+
+FILE *debugfd = NULL;
+
+int main(int argc, char **argv)
+{
+	struct line *l;
+	char buf[4096];
+	char *raw;
+	
+	while(!feof(stdin)) {
+		fgets(buf, sizeof(buf), stdin);
+		l = irc_parse_line(buf);
+		raw = irc_line_string(l);
+		puts(raw);
+		free(raw);
+		free_line(l);
+	}
+	return 0;
+}
