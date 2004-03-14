0d2b66d030f20e38006e07e84a3bdfa430edff07
Jelmer Vernooij <jelmer@samba.org>
2004-03-14 08:51:39 +0200

    Add missing RC files

---
+++ win32/resource.h
@@ -0,0 +1,84 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ctrlproxy.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CTRLPROXY_DIALOG            102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDR_MAINFRAME                   128
+#define IDD_PLUGINS                     129
+#define IDD_ABOUT                       130
+#define IDD_NETWORKS                    131
+#define IDD_LOG                         132
+#define IDD_CONFIGURATION               133
+#define IDD_STATUS                      134
+#define IDR_POPUP                       136
+#define IDD_EDIT_NETWORK                140
+#define IDD_NEW_NETWORK                 141
+#define IDD_EDIT_CHANNEL                142
+#define IDD_EDIT_LISTENER               143
+#define IDD_EDIT_SERVER                 144
+#define IDD_PHONY                       146
+#define IDC_TAB1                        1001
+#define IDC_LIST1                       1002
+#define IDC_NETWORKLIST                 1002
+#define IDC_CHANNELLIST                 1002
+#define IDC_ADD_PLUGIN                  1003
+#define IDC_CLOSE                       1004
+#define IDC_CONFTREE                    1013
+#define IDC_SAVECONFIG                  1014
+#define IDC_ADD                         1015
+#define IDC_NICKNAME                    1016
+#define IDC_USERNAME                    1017
+#define IDC_SERVERLIST                  1018
+#define IDC_NAME                        1019
+#define IDC_FULLNAME                    1026
+#define IDC_CLIENTPASS                  1027
+#define IDC_SERVERPASS                  1028
+#define IDC_LOG                         1029
+#define IDC_PLUGIN_LIST                 1031
+#define IDC_SSL                         1033
+#define IDC_AUTOCONNECT                 1034
+#define IDC_EDIT                        1035
+#define IDC_DEL                         1036
+#define IDC_NEWNETWORKNAME              1037
+#define IDC_CONNECT                     1038
+#define IDC_CLIENTLIST                  1040
+#define IDC_KICK                        1041
+#define IDC_LICENSE                     1043
+#define IDC_DISCONNECT                  1044
+#define IDC_NUMNETWORKS                 1045
+#define IDC_NUMCLIENTS                  1046
+#define IDC_NUMCHANNELS                 1047
+#define IDC_LOAD                        1048
+#define IDC_UNLOAD                      1049
+#define IDC_CONFIG                      1050
+#define IDC_ADD_CHANNEL                 1051
+#define IDC_REMOVE_CHANNEL              1052
+#define IDC_ADD_SERVER                  1053
+#define IDC_REMOVE_SERVER               1054
+#define IDC_LISTENERLIST                1055
+#define IDC_ADD_LISTENER                1056
+#define IDC_REMOVE_LISTENER             1057
+#define IDC_EDIT_CHANNEL                1058
+#define IDC_AUTOJOIN                    1059
+#define IDC_KEY                         1061
+#define IDC_PORT                        1062
+#define IDC_HOST                        1064
+#define IDM_EXIT                        32771
+#define IDM_ABOUT                       32772
+#define IDM_STATUS                      32775
+#define IDM_SHOW                        32775
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        147
+#define _APS_NEXT_COMMAND_VALUE         32781
+#define _APS_NEXT_CONTROL_VALUE         1065
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
