a360cf018ce91bd4b57743bde4d09d092b2586e7
jelmer@VERNSTOK.NL <>
2005-01-22 05:52:16 +0200

    More work on socks module

--- mods/socks.c
+++ mods/socks.c
@@ -37,6 +37,29 @@
 #define SOCKS_VERSION 0x05
 #define DEFAULT_SOCKS_PORT 1080
 
+#define SOCKS_METHOD_NOAUTH		  	0x00
+#define SOCKS_METHOD_GSSAPI 	  	0x01
+#define SOCKS_METHOD_USERNAME_PW  	0x02
+#define SOCKS_METHOD_NOACCEPTABLE 	0xFF
+
+#define ATYP_IPV4					0x01
+#define ATYP_FQDN					0x03
+#define ATYP_IPV6					0x04
+
+#define CMD_CONNECT					0x01
+#define CMD_BIND					0x02
+#define CMD_UDP_ASSOCIATE			0x03
+
+#define REP_OK						0x00
+#define REP_GENERAL_FAILURE			0x01
+#define REP_NOT_ALLOWED				0x02
+#define REP_NET_UNREACHABLE			0x03
+#define REP_HOST_UNREACHABLE		0x04
+#define REP_CONN_REFUSED			0x05
+#define REP_TTL_EXPIRED				0x06
+#define REP_CMD_NOT_SUPPORTED		0x07
+#define REP_ATYP_NOT_SUPPORTED		0x08
+
 static GIOChannel *server_channel = NULL;
 static int server_channel_in = 0;
 
@@ -44,12 +67,38 @@ struct socks_client
 {
 	GIOChannel *connection;
 	guint8 socks_version;
+	guint8 num_methods;
 	const char *user;
 	const char *password;
+	gint watch_id;
 };
+
+static gboolean handle_client_data (GIOChannel *ioc, GIOCondition o, gpointer data)
+{
+	struct socks_client *cl = data;
+	return TRUE;
+}
 
 static gboolean handle_new_client (GIOChannel *ioc, GIOCondition o, gpointer data)
 {
+	/* Spawn off new client */
+	struct socks_client *cl;
+	int ns;
+	struct sockaddr clientname;
+	size_t size;
+	
+	size = sizeof(clientname);
+	ns = accept(g_io_channel_unix_get_fd(ioc), &clientname, &size);
+	if (!ns) {
+		g_warning("Unable to accept connection");
+		return TRUE;
+	}
+	
+	cl = g_new0(struct socks_client, 1);
+	cl->connection = g_io_channel_unix_new(ns);
+	g_io_channel_set_encoding(cl->connection, NULL, NULL);
+	cl->watch_id = g_io_add_watch(cl->connection, G_IO_IN, handle_client_data, cl);
+	g_io_channel_unref(cl->connection);
 	
 	return TRUE;
 }
@@ -59,7 +108,6 @@ gboolean fini_plugin(struct plugin *p)
 {
 	/* Close port */
 	g_source_remove(server_channel_in);
-	g_io_channel_unref(server_channel);
 	return TRUE;
 }
 
@@ -72,7 +120,7 @@ gboolean init_plugin(struct plugin *p)
 	struct sockaddr_in addr;
 	guint16 port;
 
-	sock = socket(AF_INET, SOCK_STREAM, 0);
+	sock = socket(PF_INET, SOCK_STREAM, 0);
 	if (sock < 0) {
 		g_warning( "error creating socket: %s", strerror(errno));
 		return FALSE;
@@ -85,8 +133,8 @@ gboolean init_plugin(struct plugin *p)
     setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 
 	addr.sin_family = AF_INET;
-	addr.sin_port = port;
-	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	if (bind (sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
 		g_warning("Unable to bind to port %d: %s", port, strerror(errno));
@@ -106,6 +154,9 @@ gboolean init_plugin(struct plugin *p)
 	}
 
 	server_channel_in = g_io_add_watch(server_channel, G_IO_IN, handle_new_client, NULL);
+	g_io_channel_unref(server_channel);
+
+	g_message("Listening for SOCKS connections on port %d", port);
 	
 	return TRUE;
 }
