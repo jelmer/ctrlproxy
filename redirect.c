54dd32c54b2937d10efa41887aba7693c4365949
jelmer <>
2005-05-23 19:21:35 +0200

    Specify errors differently

--- redirect.c
+++ redirect.c
@@ -35,6 +35,7 @@ struct query {
 	char *name;
 	int replies[20];
 	int end_replies[20];
+	int errors[20];
 	/* Should add this query to the stack. return TRUE if this has 
 	 * been done successfully, FALSE otherwise */
 	int (*handle) (struct line *, struct query *);
@@ -47,151 +48,156 @@ static struct query queries[] = {
 /* Commands that get a one-client reply: 
  * WHOIS [<server>] <nickmask>[,<nickmask>[,...]] */
 	{"WHOIS", 
-		{ ERR_NOSUCHSERVER, RPL_WHOISUSER, RPL_WHOISCHANNELS, RPL_AWAY,
-		  RPL_WHOISIDLE, RPL_ENDOFWHOIS, ERR_NONICKNAMEGIVEN, RPL_WHOISCHANNELS,
-		  RPL_WHOISSERVER, RPL_WHOISOPERATOR, ERR_NOSUCHNICK, 0 }, 
-		{ ERR_NOSUCHSERVER, ERR_NONICKNAMEGIVEN, ERR_NOSUCHNICK, 
-		  RPL_ENDOFWHOIS, 0 }, 
+		{ RPL_WHOISUSER, RPL_WHOISCHANNELS, RPL_AWAY,
+		  RPL_WHOISIDLE, RPL_ENDOFWHOIS, RPL_WHOISCHANNELS,
+		  RPL_WHOISSERVER, RPL_WHOISOPERATOR, 0 }, 
+		{ RPL_ENDOFWHOIS, 0 }, 
+		{ ERR_NOSUCHSERVER, ERR_NONICKNAMEGIVEN, ERR_NOSUCHNICK, 0 },
 		handle_default
 	},
 
 	/* WHO [<name> [<o>]] */
 	{"WHO", 
-	    { ERR_NOSUCHSERVER, RPL_WHOREPLY, RPL_ENDOFWHO, 0 }, 
-		{ ERR_NOSUCHSERVER, RPL_ENDOFWHO, 0 },
+	    { RPL_WHOREPLY, RPL_ENDOFWHO, 0 }, 
+		{ RPL_ENDOFWHO, 0 },
+		{ ERR_NOSUCHSERVER, 0 },
 		handle_default 
 	},
 
 	/* NAMES [<channel>{,<channel>}]*/
 	{"NAMES", 
-		{ ERR_TOOMANYMATCHES, ERR_NOSUCHSERVER, RPL_NAMREPLY, RPL_ENDOFNAMES, 0 }, 
-		{ ERR_TOOMANYMATCHES, ERR_NOSUCHSERVER, RPL_ENDOFNAMES, 0 },
+		{ RPL_NAMREPLY, RPL_ENDOFNAMES, 0 }, 
+		{ RPL_ENDOFNAMES, 0 },
+		{ ERR_TOOMANYMATCHES, ERR_NOSUCHSERVER, 0 },
 		handle_default
 	},
 
  /* LIST [<channel>{,<channel>} [<server>]*/
 	{"LIST", 
-		{ ERR_TOOMANYMATCHES, ERR_NOSUCHSERVER, RPL_LIST, RPL_LISTSTART, RPL_LISTEND, 0 }, 
-		{ ERR_TOOMANYMATCHES, ERR_NOSUCHSERVER, RPL_LISTEND, 0 },
+		{ RPL_LIST, RPL_LISTSTART, RPL_LISTEND, 0 }, 
+		{ RPL_LISTEND, 0 },
+		{ ERR_TOOMANYMATCHES, ERR_NOSUCHSERVER, 0 },
 		handle_default
 	},
 	
  /* TOPIC <channel> [<topic>]*/
 	{"TOPIC", 
-		{ ERR_NEEDMOREPARAMS, RPL_NOTOPIC, ERR_NOTONCHANNEL, RPL_TOPIC, 
-		  ERR_CHANOPPRIVSNEEDED, ERR_NOCHANMODES, 0 }, 
-		{ ERR_NEEDMOREPARAMS, RPL_NOTOPIC, ERR_NOTONCHANNEL, RPL_TOPIC, 
-		  ERR_CHANOPPRIVSNEEDED, ERR_NOCHANMODES, 0 }, 
+		{ RPL_NOTOPIC, RPL_TOPIC, 0 }, 
+		{ RPL_NOTOPIC, RPL_TOPIC, 0 }, 
+		{ ERR_NOTONCHANNEL, ERR_NEEDMOREPARAMS, ERR_CHANOPPRIVSNEEDED, 
+			ERR_NOCHANMODES, 0 },
 		handle_topic
 	},
 	
  /* WHOWAS <nickname> [<count> [<server>]]*/
 	{"WHOWAS", 
-		{ ERR_NONICKNAMEGIVEN, ERR_WASNOSUCHNICK, RPL_WHOWASUSER,
-		  RPL_WHOISSERVER, RPL_ENDOFWHOWAS, 0 },
-		{ ERR_NONICKNAMEGIVEN, ERR_WASNOSUCHNICK, RPL_ENDOFWHOWAS, 
-		  RPL_WHOISSERVER, 0 },
+		{ RPL_WHOWASUSER, RPL_WHOISSERVER, RPL_ENDOFWHOWAS, 0 },
+		{ RPL_ENDOFWHOWAS, RPL_WHOISSERVER, 0 },
+		{ ERR_NONICKNAMEGIVEN, ERR_WASNOSUCHNICK, 0 },
 		handle_default
 	},
 	
  /* STATS [<query> [<server>]]*/
 	{"STATS", 
-		{ ERR_NOSUCHSERVER, RPL_STATSCLINE, RPL_STATSILINE, RPL_STATSQLINE, 
+		{ RPL_STATSCLINE, RPL_STATSILINE, RPL_STATSQLINE, 
 		  RPL_STATSLINKINFO, RPL_STATSCOMMANDS, RPL_STATSHLINE, RPL_STATSNLINE,
 		  RPL_STATSKLINE, RPL_STATSLLINE, RPL_STATSUPTIME, RPL_STATSOLINE, 
 		  RPL_ENDOFSTATS, 0 },
-		{ ERR_NOSUCHSERVER, RPL_ENDOFSTATS, 0 },
+		{ RPL_ENDOFSTATS, 0 },
+		{ ERR_NOSUCHSERVER, 0 },
 		handle_default
 	}, 
 	
  /* VERSION [<server>]*/
 	{"VERSION",
-		{ ERR_NOSUCHSERVER, RPL_VERSION, 0 },
-		{ ERR_NOSUCHSERVER, RPL_VERSION, 0 },
+		{ RPL_VERSION, 0 },
+		{ RPL_VERSION, 0 },
+		{ ERR_NOSUCHSERVER, 0 },
 		handle_default
 	}, 
 		
  /* LINKS [[<remote server>] <server mask>]*/
 	{"LINKS",
-		{ ERR_NOSUCHSERVER, RPL_LINKS, RPL_ENDOFLINKS, 0 },
-		{ ERR_NOSUCHSERVER, RPL_ENDOFLINKS, 0 },
+		{ RPL_LINKS, RPL_ENDOFLINKS, 0 },
+		{ RPL_ENDOFLINKS, 0 },
+		{ ERR_NOSUCHSERVER, 0 },
 		handle_default
 	},
 	
  /* TIME [<server>]*/
 	{"TIME",
-		{ ERR_NOSUCHSERVER, RPL_TIME, 0 },
-		{ ERR_NOSUCHSERVER, RPL_TIME, 0 },
+		{ RPL_TIME, 0 },
+		{ RPL_TIME, 0 },
+		{ ERR_NOSUCHSERVER, 0 },
 		handle_default
 	},
 
  /* TRACE [<server>]*/
 	{"TRACE",
-		{ ERR_NOSUCHSERVER, RPL_TRACELINK, RPL_TRACECONNECTING, 
+		{ RPL_TRACELINK, RPL_TRACECONNECTING, 
 		  RPL_TRACEUNKNOWN, RPL_TRACEUSER, RPL_TRACECLASS, 
 		  RPL_TRACEHANDSHAKE, RPL_TRACEOPERATOR,
 		  RPL_TRACESERVER, RPL_TRACENEWTYPE, 0 },
+		{ 0 },
 		{ ERR_NOSUCHSERVER, 0 },
 		handle_default
 	},
 	
  /* SUMMON <user> [<server>]*/
 	{"SUMMON",
-		{ ERR_NORECIPIENT, ERR_FILEERROR, ERR_NOLOGIN, ERR_NOSUCHSERVER,
-		  RPL_SUMMONING, 0 },
-		{ ERR_NORECIPIENT, ERR_FILEERROR, ERR_NOLOGIN, ERR_NOSUCHSERVER,
-		  RPL_SUMMONING, 0 },
+		{ RPL_SUMMONING, 0 },
+		{ RPL_SUMMONING, 0 },
+		{ ERR_NORECIPIENT, ERR_FILEERROR, ERR_NOLOGIN, ERR_NOSUCHSERVER, 0 },
 		handle_default
 	},
 	
  /* USERS [<server>]*/
 	{"USERS",
-		{ ERR_NOSUCHSERVER, ERR_FILEERROR, RPL_USERSSTART, RPL_USERS,
-		  RPL_NOUSERS, RPL_ENDOFUSERS, ERR_USERSDISABLED, 0 },
-		{ ERR_NOSUCHSERVER, ERR_FILEERROR, ERR_USERSDISABLED, RPL_ENDOFUSERS, 0 },
+		{ RPL_USERSSTART, RPL_USERS, RPL_NOUSERS, RPL_ENDOFUSERS, 0 },
+		{ RPL_ENDOFUSERS, 0 },
+		{ ERR_NOSUCHSERVER, ERR_FILEERROR, ERR_USERSDISABLED, 0 },
 		handle_default
 	},
 	
  /* USERHOST <nickname>{ <nickname>}{ ...}*/
 	{"USERHOST",
-		{ RPL_USERHOST, ERR_NEEDMOREPARAMS, 0 },
-		{ RPL_USERHOST, ERR_NEEDMOREPARAMS, 0 },
+		{ RPL_USERHOST, 0 },
+		{ RPL_USERHOST, 0 },
+		{ ERR_NEEDMOREPARAMS, 0 },
 		handle_default
 	},
 		
  /* ISON <nickname>{ <nickname>}{ ...} */
 	{"ISON",
-		{ RPL_ISON, ERR_NEEDMOREPARAMS, 0 },
-		{ RPL_ISON, ERR_NEEDMOREPARAMS, 0 },
+		{ RPL_ISON, 0 },
+		{ RPL_ISON, 0 },
+		{ ERR_NEEDMOREPARAMS, 0 },
 	    handle_default
 	},
 
 /* JOIN <channel>{,<channel>} [<key>{,<key>}] */
 	{"JOIN",
+		{ RPL_TOPIC, 0 },
+		{ RPL_TOPIC, 0 },
 		{ ERR_NEEDMOREPARAMS, ERR_BANNEDFROMCHAN,
 		  ERR_INVITEONLYCHAN, ERR_BADCHANNELKEY,
 		  ERR_CHANNELISFULL, ERR_BADCHANMASK,
-		  ERR_NOSUCHCHANNEL, ERR_TOOMANYCHANNELS,
-		  RPL_TOPIC, 0 },
-		{ ERR_NEEDMOREPARAMS, ERR_BANNEDFROMCHAN,
-		  ERR_INVITEONLYCHAN, ERR_BADCHANNELKEY,
-		  ERR_CHANNELISFULL, ERR_BADCHANMASK,
-		  ERR_NOSUCHCHANNEL, ERR_TOOMANYCHANNELS,
-		  RPL_TOPIC, 0 },
+		  ERR_NOSUCHCHANNEL, ERR_TOOMANYCHANNELS, 0 },
 		handle_default
 	},
 
  /* PART <channel> *( "," <channel> ) [ <Part Message> ] */
 	{ "PART",
-		{ ERR_NEEDMOREPARAMS, ERR_NOSUCHCHANNEL, ERR_NOTONCHANNEL, 0 },
+		{ 0 },
+		{ 0 },
 		{ ERR_NEEDMOREPARAMS, ERR_NOSUCHCHANNEL, ERR_NOTONCHANNEL, 0 },
 		handle_default
 	},
 
  /* NICK <nickname> */
 	{ "NICK",
-		{ ERR_NONICKNAMEGIVEN, ERR_ERRONEUSNICKNAME, ERR_NICKNAMEINUSE,
-	      ERR_UNAVAILRESOURCE, ERR_RESTRICTED, ERR_NICKCOLLISION, 0 },
+		{ 0 },
+		{ 0 },
 		{ ERR_NONICKNAMEGIVEN, ERR_ERRONEUSNICKNAME, ERR_NICKNAMEINUSE,
 	      ERR_UNAVAILRESOURCE, ERR_RESTRICTED, ERR_NICKCOLLISION, 0 },
 		handle_default
@@ -199,52 +205,50 @@ static struct query queries[] = {
 
  /* OPER <name> <password> */
 	{ "OPER",
-	    { ERR_NEEDMOREPARAMS, RPL_YOUREOPER, 
-			ERR_NOOPERHOST, ERR_PASSWDMISMATCH, 0 },
-	    { ERR_NEEDMOREPARAMS, RPL_YOUREOPER, 
-			ERR_NOOPERHOST, ERR_PASSWDMISMATCH, 0 },
+	    { RPL_YOUREOPER, 0 },
+	    { RPL_YOUREOPER, 0 },
+		{ ERR_NEEDMOREPARAMS, ERR_NOOPERHOST, ERR_PASSWDMISMATCH, 0 },
 		handle_default
 	},
 	
  /* MODE <nick> <mode> */
 	{ "MODE", 
 		/* FIXME: Channel modes */
-		{ ERR_NEEDMOREPARAMS, ERR_UMODEUNKNOWNFLAG, 
-			RPL_UMODEIS, ERR_USERSDONTMATCH, 0 },
-		{ ERR_NEEDMOREPARAMS, ERR_UMODEUNKNOWNFLAG, 
-			RPL_UMODEIS, ERR_USERSDONTMATCH, 0 },
+		{ RPL_UMODEIS, 0 },
+		{ RPL_UMODEIS, 0 },
+		{ ERR_NEEDMOREPARAMS, ERR_UMODEUNKNOWNFLAG, ERR_USERSDONTMATCH, 0 },
 		handle_default
 	},
 	
  /* SERVICE <nick> <reserved> <distribution> <type> <reserved> <info> */
 	{ "SERVICE", 
-		{ ERR_ALREADYREGISTERED, ERR_NEEDMOREPARAMS, ERR_ERRONEUSNICKNAME,
-			RPL_YOURESERVICE, RPL_YOURHOST, RPL_MYINFO, 0 },
-		{ ERR_ALREADYREGISTERED, ERR_NEEDMOREPARAMS, ERR_ERRONEUSNICKNAME,
-			RPL_YOURESERVICE, RPL_YOURHOST, RPL_MYINFO, 0 },
+		{ RPL_YOURESERVICE, RPL_YOURHOST, RPL_MYINFO, 0 },
+		{ RPL_YOURESERVICE, RPL_YOURHOST, RPL_MYINFO, 0 },
+		{ ERR_ALREADYREGISTERED, ERR_NEEDMOREPARAMS, ERR_ERRONEUSNICKNAME, 0 },
 		handle_default
 	},
 	
  /* SQUIT <server> <comment> */
 	{ "SQUIT",
-		{ ERR_NOPRIVILEGES, ERR_NEEDMOREPARAMS, ERR_NOSUCHSERVER, 0 },
+		{ 0 },
+		{ 0 },
 		{ ERR_NOPRIVILEGES, ERR_NEEDMOREPARAMS, ERR_NOSUCHSERVER, 0 },
 		handle_default
 	},
 	
  /* INVITE <nick> <channel> */
 	{ "INVITE", 
+		{ RPL_INVITING, RPL_AWAY, 0 },
+		{ RPL_INVITING, RPL_AWAY, 0 },
 		{ ERR_NEEDMOREPARAMS, ERR_NOTONCHANNEL, ERR_NOSUCHNICK, 
-			ERR_CHANOPPRIVSNEEDED, ERR_USERONCHANNEL, RPL_INVITING, RPL_AWAY, 0 },
-		{ ERR_NEEDMOREPARAMS, ERR_NOTONCHANNEL, ERR_NOSUCHNICK, 
-			ERR_CHANOPPRIVSNEEDED, ERR_USERONCHANNEL, RPL_INVITING, RPL_AWAY, 0 },
+			ERR_CHANOPPRIVSNEEDED, ERR_USERONCHANNEL, 0 },
 		handle_default
 	},
 	
 /* KICK <channel> * ( "," <channel> ) <user> *( "," <user> ) [<comment>] */
 	{ "KICK",
-		{ ERR_NEEDMOREPARAMS, ERR_BADCHANMASK, ERR_USERNOTINCHANNEL,
-			ERR_NOSUCHCHANNEL, ERR_CHANOPPRIVSNEEDED, ERR_NOTONCHANNEL, 0 },
+		{ 0 },
+		{ 0 },
 		{ ERR_NEEDMOREPARAMS, ERR_BADCHANMASK, ERR_USERNOTINCHANNEL,
 			ERR_NOSUCHCHANNEL, ERR_CHANOPPRIVSNEEDED, ERR_NOTONCHANNEL, 0 },
 		handle_default
@@ -252,49 +256,53 @@ static struct query queries[] = {
  
  /* PRIVMSG <msgtarget> <text> */
 	{ "PRIVMSG",
+		{ RPL_AWAY, 0 },
+		{ RPL_AWAY, 0 },
 		{ ERR_NORECIPIENT, ERR_NOTEXTTOSEND, ERR_CANNOTSENDTOCHAN, 
 			ERR_NOTOPLEVEL, ERR_TOOMANYTARGETS, ERR_WILDTOPLEVEL, 
-			ERR_NOSUCHNICK, RPL_AWAY, 0 },
-		{ ERR_NORECIPIENT, ERR_NOTEXTTOSEND, ERR_CANNOTSENDTOCHAN, 
-			ERR_NOTOPLEVEL, ERR_TOOMANYTARGETS, ERR_WILDTOPLEVEL, 
-			ERR_NOSUCHNICK, RPL_AWAY, 0 },
+			ERR_NOSUCHNICK, 0 },
 		handle_default
 	},
  
  /* MOTD [<target>] */
 	{ "MOTD",
-		{ RPL_MOTDSTART, RPL_MOTD, ERR_NOMOTD, RPL_ENDOFMOTD, 0 },
-		{ ERR_NOMOTD, RPL_ENDOFMOTD, 0 },
+		{ RPL_MOTDSTART, RPL_MOTD, RPL_ENDOFMOTD, 0 },
+		{ RPL_ENDOFMOTD, 0 },
+		{ ERR_NOMOTD, 0 },
 		handle_default
 	},
 
  /* LUSERS [ <mask> [ <target> ] ] */
 	{ "LUSERS",
 		{ RPL_LUSERCLIENT, RPL_LUSEROP, RPL_LUSERUNKNOWN, RPL_LUSERCHANNELS, 
-			RPL_LUSERME, ERR_NOSUCHSERVER, 0 },
+			RPL_LUSERME, 0 },
+		{ 0 },
 		{ ERR_NOSUCHSERVER, 0 },
 		handle_default
 	},
 	
  /* CONNECT <target> <port> [ <remote server> ] */
 	{ "CONNECT",
-		{ ERR_NOSUCHSERVER, ERR_NEEDMOREPARAMS, ERR_NOPRIVILEGES, 0 },
+		{ 0 },
+		{ 0 },
 		{ ERR_NOSUCHSERVER, ERR_NEEDMOREPARAMS, ERR_NOPRIVILEGES, 0 },
 		handle_default
 	},
 
  /* ADMIN [ <target> ] */
 	{ "ADMIN",
-		{ ERR_NOSUCHSERVER, RPL_ADMINME, RPL_ADMINLOC2, RPL_ADMINLOC1, 
+		{ RPL_ADMINME, RPL_ADMINLOC2, RPL_ADMINLOC1, 
 			RPL_ADMINLOC3, 0 },
+		{ 0 },
 		{ ERR_NOSUCHSERVER, 0 },
 		handle_default
 	},
 	
  /* INFO [ <target> ] */
 	{ "INFO",
-		{ ERR_NOSUCHSERVER, RPL_INFO, RPL_ENDOFINFO, 0 },
-		{ ERR_NOSUCHSERVER, RPL_ENDOFINFO, 0 },
+		{ RPL_INFO, RPL_ENDOFINFO, 0 },
+		{ RPL_ENDOFINFO, 0 },
+		{ ERR_NOSUCHSERVER, 0 },
 		handle_default
 	},
 	
@@ -302,24 +310,25 @@ static struct query queries[] = {
 	{ "SERVLIST",
 		{ RPL_SERVLIST, RPL_SERVLISTEND, 0},
 		{ RPL_SERVLISTEND, 0 },
+		{ 0 },
 		handle_default
 	},
 	
  /* SQUERY <servicename> <text> */
 	/* Same responses as for PRIVMSG */
 	{ "SQUERY",
+		{ RPL_AWAY, 0 },
+		{ RPL_AWAY, 0 },
 		{ ERR_NORECIPIENT, ERR_NOTEXTTOSEND, ERR_CANNOTSENDTOCHAN, 
 			ERR_NOTOPLEVEL, ERR_TOOMANYTARGETS, ERR_WILDTOPLEVEL, 
-			ERR_NOSUCHNICK, RPL_AWAY, 0 },
-		{ ERR_NORECIPIENT, ERR_NOTEXTTOSEND, ERR_CANNOTSENDTOCHAN, 
-			ERR_NOTOPLEVEL, ERR_TOOMANYTARGETS, ERR_WILDTOPLEVEL, 
-			ERR_NOSUCHNICK, RPL_AWAY, 0 },
+			ERR_NOSUCHNICK, 0 },
 		handle_default
 	},
 	
  /* KILL <nick> <comment> */
 	{ "KILL",
-		{ ERR_NOPRIVILEGES, ERR_NEEDMOREPARAMS, ERR_NOSUCHNICK, ERR_CANTKILLSERVER, 0 },
+		{ 0 },
+		{ 0 },
 		{ ERR_NOPRIVILEGES, ERR_NEEDMOREPARAMS, ERR_NOSUCHNICK, ERR_CANTKILLSERVER, 0 },
 		handle_default
 	},
@@ -328,33 +337,38 @@ static struct query queries[] = {
 	{ "AWAY",
 		{ RPL_UNAWAY, RPL_NOWAWAY, 0 },
 		{ RPL_UNAWAY, RPL_NOWAWAY, 0 },
+		{ 0 },
 		handle_default
 	},
 
  /* REHASH */
 	{ "REHASH",
-		{ RPL_REHASHING, ERR_NOPRIVILEGES, 0 },
-		{ RPL_REHASHING, ERR_NOPRIVILEGES, 0 },
+		{ RPL_REHASHING, 0 },
+		{ RPL_REHASHING, 0 },
+		{ ERR_NOPRIVILEGES, 0 },
 		handle_default
 	},
 
  /* DIE */
 	{ "DIE",
-		{ ERR_NOPRIVILEGES, 0 },
+		{ 0 },
+		{ 0 },
 		{ ERR_NOPRIVILEGES, 0 },
 		handle_default
 	},
 
  /* RESTART */
 	{ "RESTART",
-		{ ERR_NOPRIVILEGES, 0 },
+		{ 0 },
+		{ 0 },
 		{ ERR_NOPRIVILEGES, 0 },
 		handle_default
 	},
 
  /* WALLOPS */
 	{ "WALLOPS",
-		{ ERR_NEEDMOREPARAMS, 0 },
+		{ 0 },
+		{ 0 },
 		{ ERR_NEEDMOREPARAMS, 0 },
 		handle_default
 	},
@@ -396,14 +410,16 @@ void redirect_response(struct network *network, struct line *l)
 
 	/* Find a request that this response is a reply to */
 	while(s) {
-		if(is_reply(s->query->replies, n) && s->network == l->network) {
+		if(s->network == l->network && 
+		   (is_reply(s->query->replies, n) || is_reply(s->query->errors, n))) {
 			/* Send to client that queried, if that client still exists */
 			if(verify_client(s->network, s->client)) {
 				c = s->client;
 				client_send_line(s->client, l);
 			}
 
-			if(is_reply(s->query->end_replies, n)) {
+			if(is_reply(s->query->end_replies, n) || 
+			   is_reply(s->query->errors, n)) {
 				/* Remove from stack */
 				if(!p)stack = s->next;	
 				else p->next = s->next;
