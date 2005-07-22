2128d3dadd8a666d019eb7140d3523599a410090
jelmer <>
2005-07-22 07:57:26 +0200

    Couple of small updates.

--- mods/linestack_file.c
+++ mods/linestack_file.c
@@ -35,6 +35,12 @@
 #define mkdir(s,t) _mkdir(s)
 #endif
 
+struct record_header {
+	guint32 offset;
+	time_t time;
+	guint32 length;
+};
+
 static char *data_path = NULL;
 static GHashTable *networks = NULL;
 
@@ -74,18 +80,44 @@ static gboolean file_fini(void)
 	return TRUE;
 }
 
+static gboolean file_insert_state(FILE *ch, const struct network_state *state)
+{
+	char *raw = network_state_encode(state);
+	struct record_header rh;
+	
+	rh.time = time(NULL);
+	rh.length = strlen(raw);
+	rh.offset = 0;
+
+	if (fwrite(&rh, sizeof(rh), 1, ch) != 1)
+		return FALSE;
+
+	fprintf(ch, "%s\n", raw);
+
+	g_free(raw);
+
+	return TRUE;
+}
+
 static gboolean file_insert_line(const struct network *n, const struct line *l)
 {
 	FILE *ch = get_fd(n);
 	char *raw;
 	int ret;
-	time_t t = time(NULL);
+	struct record_header rh;
+	
 	if (!ch) return FALSE;
 
-	if (fwrite(&t, sizeof(t), 1, ch) != 1)
+	/* FIXME: decide whether file_insert_state should be called */
+
+	rh.time = time(NULL);
+	rh.offset = 0; /* FIXME: Get offset */
+	raw = irc_line_string_nl(l);
+	rh.length = strlen(raw);
+	
+	if (fwrite(&rh, sizeof(rh), 1, ch) != 1)
 		return FALSE;
 
-	raw = irc_line_string_nl(l);
 	ret = fputs(raw, ch);
 	g_free(raw);
 
@@ -108,9 +140,40 @@ static 	struct network_state * file_get_state (
 		struct network *n, 
 		linestack_marker *m)
 {
-	return NULL; /* FIXME */
-}
+	FILE *ch = get_fd(n);
+	struct record_header rh;
+	struct network_state *ret;
+	char *raw;
+	long from_offset, to_offset = ftell(ch);
+		
+	/* Read offset at marker position */
+	if (fread(&rh, sizeof(rh), 1, ch) != 1)
+		return NULL;
 
+	from_offset = rh.offset;
+
+	/* fseek to state dump */
+	fseek(ch, SEEK_CUR, -1*from_offset);
+	
+	if (fread(&rh, sizeof(rh), 1, ch) != 1)
+		return NULL;
+
+	g_assert(rh.offset == 0);
+	raw = g_malloc(rh.length+1);
+
+	if (fgets(raw, rh.length, ch) == NULL)
+		return FALSE;
+
+	raw[rh.length] = '\0';
+
+	ret = network_state_decode(raw);
+
+	g_free(raw);
+
+	linestack_replay(n, &from_offset, &to_offset, ret);
+	
+	return ret;
+}
 
 static gboolean file_traverse(struct network *n, 
 		linestack_marker *mf,
@@ -118,31 +181,42 @@ static gboolean file_traverse(struct network *n,
 		linestack_traverse_fn tf, 
 		void *userdata)
 {
-	char raw[512];
-	long *offset;
+	long *start_offset, *end_offset;
 	FILE *ch = get_fd(n);
 	if (!ch) return FALSE;
 
 	/* Flush channel before reading otherwise data corruption may occur */
 	fflush(ch);
 	
-	offset = mf;
-
-	/* FIXME: obey mt */
+	start_offset = mf;
+	end_offset = mt;
 
 	/* Go back to begin of file */
-	fseek(ch, offset?*offset:0, SEEK_SET);
+	fseek(ch, start_offset?*start_offset:0, SEEK_SET);
 	
-	while(!feof(ch)) 
+	while(!feof(ch) && (!end_offset || ftell(ch) < *end_offset)) 
 	{
-		time_t t;
+		struct record_header rh;
+		char *raw;
 		struct line *l;
-		if (fread(&t, sizeof(t), 1, ch) != 1)
+		
+		if (fread(&rh, sizeof(rh), 1, ch) != 1)
 			return FALSE;
-		if (fgets(raw, sizeof(raw), ch) == NULL)
+
+		if (rh.offset == 0) {
+			fseek(ch, rh.length, SEEK_CUR);
+			continue;
+		}
+
+		raw = g_malloc(rh.length+1);
+		
+		if (fgets(raw, rh.length, ch) == NULL)
 			return FALSE;
+
+		raw[rh.length] = '\0';
+
 		l = irc_parse_line(raw);
-		tf(l, t, userdata);
+		tf(l, rh.time, userdata);
 		free_line(l);
 	}
 
