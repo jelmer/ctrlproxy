1eaee6984b11a0701f3896f924d2ad3e5798ed55
Jelmer Vernooij <jelmer@samba.org>
2006-04-17 22:36:15 +0200

    Get rid of python module (for now)

--- example/admin_cmd.py
+++
@@ -1,6 +0,0 @@
-import ctrlproxy
-
-def command_handler(x1):
-	print "Works!"
-
-ctrlproxy.register_command('mycommand', command_handler, 'short help', 'long help')
--- example/listener.py
+++
@@ -1,8 +0,0 @@
-import ctrlproxy
-import ctrlproxy.listener
-
-l = ctrlproxy.listener.Listener(3000)
-l.password = "secret"
-l.start()
-print l.port
-l.stop()
