6dbed22723a433247a795b65e7146a3b4501148e
Jelmer Vernooij <jelmer@samba.org>
2006-07-07 17:09:57 +0200

    Support reply 487

--- src/redirect.c
+++ src/redirect.c
@@ -466,7 +466,8 @@ static void handle_464(struct network *n, struct line *l)
 
 /* List of responses that should be sent to all clients */
 static int response_all[] = { RPL_NOWAWAY, RPL_UNAWAY, RPL_NAMREPLY, 
-	RPL_ENDOFNAMES, ERR_NEEDREGGEDNICK, RPL_UMODEIS, 0 };
+	RPL_ENDOFNAMES, ERR_NEEDREGGEDNICK, RPL_UMODEIS, 
+	ERR_NO_OP_SPLIT, 0 };
 static int response_none[] = { ERR_NOMOTD, RPL_ENDOFMOTD, 0 };
 static struct {
 	int response;
