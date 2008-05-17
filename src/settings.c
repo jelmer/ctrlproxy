d01ca7b76d4273f7d2feaf310523dcd8418e6781
Jelmer Vernooij <jelmer@samba.org>
2008-05-17 19:53:11 +0200

    Fix typo for learn-nickserv variable.

--- src/settings.c
+++ src/settings.c
@@ -1202,7 +1202,7 @@ struct ctrlproxy_config *load_configuration(const char *dir)
 	}
 
     if (g_key_file_has_key(kf, "global", "learn-nickserv", NULL))
-		cfg->learn_nickserv = g_key_file_get_boolean(kf, "global", "learn-nicksev", NULL);
+		cfg->learn_nickserv = g_key_file_get_boolean(kf, "global", "learn-nickserv", NULL);
     else 
 	    cfg->learn_nickserv = TRUE;
 
