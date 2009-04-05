23d41a70c0235678d4ca3016f9244e82367537a1
Jelmer Vernooij <jelmer@samba.org>
2009-04-05 17:49:13 +0200

    Add callback for network disconnect.

--- libirc/connection.h
+++ libirc/connection.h
@@ -101,6 +101,7 @@ struct irc_network_callbacks {
 	network_log_fn log;
 	struct irc_login_details *(*get_login_details) (struct irc_network *);
 	gboolean (*process_from_server) (struct irc_network *, const struct irc_line *);
+	void (*disconnect) (struct irc_network *);
 };
 
 /**
